299728c57f20cc02bf567a15cacd54dcdf8dc02b
Narcis Brindusescu <Narcis75@gmail.com>
2020-06-10 18:25:59 +0300

    merge dela pC

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel (Narcis-PC)
                 </div>
 
                 <div class="links">

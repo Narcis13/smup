c5c9a7d8650dac68d2d4a8d3be0751201c816240
Narcis Brindusescu <narcisbrindusescu@Narciss-Mac-mini.local>
2020-06-18 15:11:08 +0300

    greu

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -18,13 +18,35 @@
     
         <div class="flex">
            <img class="h-16 m-2"src="/img/sigla.jpg" alt="sigla">
+           <img class="h-16 m-2"src="/img/logoanmcs_mica.png" alt="sigla">
            <ul class="flex items-center justify-around uppercase w-full">
                <li>
-               <a class="block" href="">Despre noi</a> 
+               <a class="block " href="#">Despre noi</a> 
+                        <ul class="absolute">
+                           <li>
+                                <a href="#">Istoric</a>
+                           </li>
+                           <li>
+                                <a href="#">Misiune si valori</a>
+                           </li>
+                           <li>
+                                <a href="#">Conducere</a>
+                           </li>
+                        </ul>
                </li>
                <li>
-               <a class="block" href="">Management</a>
-          
+               <a class="block" href="#">Management</a>
+                        <ul class="absolute pt-8 text-base">
+                           <li >
+                                <a class="p-1 block border-b-1 border-gray-600" href="#">Evenimente</a>
+                           </li>
+                           <li >
+                                <a class="p-1 block border-b-1 border-gray-600" href="#">Plan de management</a>
+                           </li>
+                           <li >
+                                <a class="p-1 block border-b-1 border-gray-600" href="#">Strategia de dezvoltare</a>
+                           </li>
+                        </ul>
                </li>
                <li>
                <a class="block" href="">Resurse umane</a>
